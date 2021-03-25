e3498596397757b414d41d74a2addf0455f2cbd4
André Cavalcante <6445989+andrecavalcante@users.noreply.github.com>
2021-03-25 17:01:44 -0300

    Add files via upload

--- iwae.py
+++ iwae.py
@@ -54,27 +54,26 @@ class IWAE(nn.Module):
         x = x.unsqueeze(1) # unsqueeze for broadcast
         log_px_given_h = torch.sum(x * torch.log(likelihood_mean) + (1-x) * torch.log(1 - likelihood_mean), dim=-1) # sum over num_samples
 
-        # gaussian Prior p(h)
+        # gaussian prior p(h)
         log_ph = torch.sum(-0.5* torch.log(torch.tensor(2*np.pi)) - torch.pow(0.5*h,2), dim=-1) # sum over num_samples
 
         # evaluation of a gaussian proposal q(h|x)
         log_qh_given_x = torch.sum(-0.5* torch.log(torch.tensor(2*np.pi))-torch.log(q_std) - 0.5*torch.pow((h-q_mean)/q_std, 2), dim=-1)
         
-        # Computing log weights 
+        # computing log weights 
         log_w = log_px_given_h + log_ph - log_qh_given_x
-        # handling under-flow 
-        log_w = log_w - torch.max(log_w, dim=-1)[0].unsqueeze(1)
-        
-        # computing normalized weights
-        normalized_w =  torch.exp(log_w)/ torch.sum(torch.exp(log_w), dim=-1).unsqueeze(1) # unsqueeze sum for broadcast
+       
+        # normalized weights through Exp-Normalization trick
+        M = torch.max(log_w, dim=-1)[0].unsqueeze(1)
+        normalized_w =  torch.exp(log_w - M)/ torch.sum(torch.exp(log_w - M), dim=-1).unsqueeze(1) # unsqueeze for broadcast
 
         # loss signal        
         loss = torch.sum(normalized_w.detach().data * (log_px_given_h + log_ph - log_qh_given_x), dim=-1) 
         loss = -torch.mean(loss) # mean over batchs
 
-        # computing log likelihood
-        w = torch.exp(log_w)
-        log_px = torch.mean(torch.log((1/num_samples)*torch.sum(w, dim=-1))) # mean over batches
+        # computing log likelihood through Log-Sum-Exp trick
+        log_px = M + torch.log((1/num_samples)*torch.sum(torch.exp(log_w - M), dim=-1))
+        log_px = torch.mean(log_px) # mean over batches
         
         return likelihood_mean, log_px, loss
  
@@ -83,10 +82,10 @@ def main():
     x_dim = 28*28
     h_dim = 50
     num_samples = 5
-    num_epochs = 50
+    num_epochs = 35
     lr = 10e-4
 
-    train_dataset = torchvision.datasets.MNIST(root='/home/andre/Dropbox/iwae',train=True, transform=transforms.ToTensor(), download=True)
+    train_dataset = torchvision.datasets.MNIST(root='C:/Users/Andre/Dropbox/iwae',train=True, transform=transforms.ToTensor(), download=True)
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
     batch_size=batch_size,shuffle=True)
 
@@ -102,8 +101,8 @@ def main():
             loss.backward()
             optimizer.step()
 
-        print('Epoch [{}/{}],  Loss: {:.3f}'.format(epoch + 1, num_epochs, loss.item()))
-        print('Epoch [{}/{}],  Log-likelihood: {:.3f}'.format(epoch + 1, num_epochs, log_px.item()))
+        print('Epoch [{}/{}],  loss: {:.3f}'.format(epoch + 1, num_epochs, loss.item()))
+        print('Epoch [{}/{}],  negative log-likelihood: {:.3f}'.format(epoch + 1, num_epochs, - log_px.item()))
 
     plt.imshow(reconstructed_x[0,0].detach().cpu().numpy().reshape(28,28))
     plt.show()
